6573652a3d8397010198ef740c768cdaea13cae0
shaoming Huang <msn8971@gmail.com>
2017-08-30 14:46:06 +0800

    .net_enum

---
+++ test0830_enum/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test0830_enum {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        enum TSeason {
+            Spring = 1,
+            Summer = 2,
+            Winter = 3
+        }
+        private void button1_Click(object sender, EventArgs e) {
+
+            
+            int x = (int)TSeason.Spring;
+            button1.Text = x.ToString();
+            //button1.Text = TSeason.Spring.ToString();  //列舉內的文字
+            //button1.Text = Convert.ToInt32(TSeason.Spring).ToString(); //列舉內的位置
+        }
+    }
+}
